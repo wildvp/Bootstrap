7d42283bcce7309d44f48d999957010aed1edf6f
Carlos Muniz <professorcarlosmuniz@gmail.com>
2020-08-09 09:04:00 -0300

    Add files via upload

---
+++ alertas.php
@@ -0,0 +1,198 @@
+<?php include_once "topo.php";?>
+        
+        
+       
+
+        
+        <!-- ======================================================================================================= -->
+        
+         <!--ALERTAS-->
+         
+         <!-- ***CÓDIGOS DE ALERTA DENTRO DO CONTAINER***** -->
+        
+        <div class="container">
+        	<div class="row justify-content-center">
+            
+            	<div class="col-12">
+            
+            	                               
+                	<!--ALERTA 01-->
+                
+                	<div class="alert alert-primary" role="alert">
+                		Mensagem do alerta
+                	</div>
+                    
+                    
+                </div>
+            
+          </div>
+          
+          <!-- ***CÓDIGOS DE ALERTA DENTRO DO CONTAINER***** -->
+          
+          <div class="container">
+            
+          		<div class="row justify-content-center">
+            
+            			<div class="col-12">
+            
+            	                               
+                	
+                			<!--ALERTA 02-->
+                    
+                			<div class="alert alert-secondary" role="alert">
+                				Mensagem do alerta
+                			</div>
+                    
+                    
+                		</div>
+           
+            	</div>
+         	</div>
+            
+        
+        
+        
+        
+          <!-- ***CÓDIGOS DE ALERTA DENTRO DO CONTAINER***** -->
+          
+          <div class="container">
+            
+          		<div class="row justify-content-center">
+            
+            			<div class="col-12">
+            
+            	                               
+                	
+                			<!--ALERTA 03-->
+                    
+                			<div class="alert alert-success" role="alert">
+                				Mensagem do alerta
+                			</div>
+                    
+                    
+                		</div>
+           
+            	</div>
+         	</div>
+            
+            
+          <!-- ***CÓDIGOS DE ALERTA DENTRO DO CONTAINER***** -->
+          
+          <div class="container">
+            
+          		<div class="row justify-content-center">
+            
+            			<div class="col-12">
+            
+            	                               
+                	
+                			<!--ALERTA 04-->
+                    
+                			<div class="alert alert-danger" role="alert">
+                				Mensagem do alerta
+                			</div>
+                    
+                    
+                		</div>
+           
+            	</div>
+         	</div>
+            
+            
+
+          <!-- ***CÓDIGOS DE ALERTA DENTRO DO CONTAINER***** -->
+          
+          <div class="container">
+            
+          		<div class="row justify-content-center">
+            
+            			<div class="col-12">
+            
+            	                               
+                	
+                			<!--ALERTA 05-->
+                    
+                			<div class="alert alert-warning" role="alert">
+                				Mensagem do alerta
+                			</div>
+                    
+                    
+                		</div>
+           
+            	</div>
+         	</div>
+            
+            
+            
+          <!-- ***CÓDIGOS DE ALERTA DENTRO DO CONTAINER***** -->
+          
+          <div class="container">
+            
+          		<div class="row justify-content-center">
+            
+            			<div class="col-12">
+            
+            	                               
+                	
+                			<!--ALERTA 06-->
+                    
+                			<div class="alert alert-info" role="alert">
+                				Mensagem do alerta
+                			</div>
+                    
+                    
+                		</div>
+           
+            	</div>
+         	</div>
+            
+            
+          <!-- ***CÓDIGOS DE ALERTA DENTRO DO CONTAINER***** -->
+          
+          <div class="container">
+            
+          		<div class="row justify-content-center">
+            
+            			<div class="col-12">
+            
+            	                               
+                	
+                			<!--ALERTA 07-->
+                    
+                			<div class="alert alert-light" role="alert">
+                				Mensagem do alerta
+                			</div>
+                    
+                    
+                		</div>
+           
+            	</div>
+         	</div>
+            
+            
+                      <!-- ***CÓDIGOS DE ALERTA DENTRO DO CONTAINER***** -->
+          
+          <div class="container">
+            
+          		<div class="row justify-content-center">
+            
+            			<div class="col-12">
+            
+            	                               
+                	
+                			<!--ALERTA 08-->
+                    
+                			<div class="alert alert-dark" role="alert">
+                				Mensagem do alerta
+                			</div>
+                    
+                    
+                		</div>
+           
+            	</div>
+         	</div>
+                 
+         
+         
+        
+<?php include_once "footer.php";?>
---
+++ grids.php
@@ -0,0 +1,31 @@
+<?php include_once "topo.php";?>
+        
+        
+        <!-- LINHA 1-->
+        
+        <div class="container">
+        	<div class="row justify-content-center">
+            
+            	<!--COLUNAS-->
+                <div class="col texto_site justify-content-center">
+                
+                
+                
+                	
+                
+                             
+                
+
+                </div>
+                
+                
+            
+            </div>
+        </div>
+        
+       
+                 
+         
+         
+        
+<?php include_once "footer.php";?>
