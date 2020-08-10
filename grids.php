7b6bf5cb2450d8460279589ea102f610919a0c80
Carlos Muniz <professorcarlosmuniz@gmail.com>
2020-08-10 18:28:11 -0300

    Add files via upload

--- grids.php
+++ grids.php
@@ -1,31 +1,193 @@
 <?php include_once "topo.php";?>
         
         
-        <!-- LINHA 1-->
+<!-- GRIDS CONTAINERS NORMAIS UMA COLUNA-->
+<!-- OBS.: justify-content-center  SERVE PARA CENTRALIZAR NOP NAVEGADOR (Pode ser a esquerda, direita-->
         
         <div class="container">
         	<div class="row justify-content-center">
             
-            	<!--COLUNAS-->
+            	
                 <div class="col texto_site justify-content-center">
+                           
                 
-                
-                
-                	
-                
-                             
+
+                </div>
+          
+            </div>
+        </div>
+        
+ <!-- GRIDS CONTAINERS NORMAIS MAIS DE UMA COLUNA basta adicionar mais cols dentro de um container (Max=12) -->
+        
+        <div class="container">
+        	<div class="row justify-content-center">
+            
+            	
+                <div class="col texto_site justify-content-center">
+                           
                 
 
                 </div>
                 
+                <div class="col texto_site justify-content-center">
+                           
                 
-            
+
+                </div>
+          
             </div>
         </div>
         
+<!-- GRIDS CONTAINERS FLUID UMA COLUNA-->
+        
+        <div class="container-fluid">
+        	<div class="row justify-content-center">
+            
+            	
+                <div class="col texto_site justify-content-center">
+                           
+                
+
+                </div>
+          
+            </div>
+        </div>
+        
+        
+        
+ <!-- GRIDS CONTAINERS FLUIS MAIS DE UMA COLUNA basta adicionar mais cols dentro de um container (Max=12) -->
+        
+        <div class="container-fluid">
+        	<div class="row justify-content-center">
+            
+            	
+                <div class="col texto_site justify-content-center">
+                           
+                
+
+                </div>
+                
+                <div class="col texto_site justify-content-center">
+                           
+                
+
+                </div>
+          
+            </div>
+        </div>
+        
+  <!-- GRIDS CONTAINERS NORMAIS MAIS DE UMA COLUNA basta adicionar mais cols detro de um container (Max=12) TAMANHOS DIFERENTES -->
+        
+        <div class="container">
+        	<div class="row justify-content-center">
+            
+            	
+                <div class="col-6 texto_site justify-content-center">
+                           
+               
+                </div>
+                
+                <div class="col texto_site justify-content-center">
+                           
+                
+
+                </div>
+                
+                <div class="col texto_site justify-content-center">
+                           
+                
+
+                </div>
+          
+            </div>
+        </div>     
+
+  <!-- GRIDS CONTAINERS NORMAIS MAIS DE UMA COLUNA basta adicionar mais cols detro de um container (Max=12) TAMANHOS DIFERENTES -->
+        
+        <div class="container">
+        	<div class="row justify-content-center">
+            
+            	
+                <div class="col-1 texto_site justify-content-center">
+                           
+               
+                </div>
+                
+                <div class="col texto_site justify-content-center">
+                           
+                
+
+                </div>
+                
+                <div class="col texto_site justify-content-center">
+                           
+                
+
+                </div>
+          
+            </div>
+        </div> 
+        
+   <!-- GRIDS CONTAINERS NORMAIS MAIS DE UMA COLUNA basta adicionar mais cols detro de um container (Max=12) TAMANHOS DIFERENTES -->
+        
+        <div class="container">
+        	<div class="row justify-content-center">
+            
+            	
+                <div class="col-6 texto_site justify-content-center">
+                           
+               
+                </div>
+                
+                <div class="col-1 texto_site justify-content-center">
+                           
+                
+
+                </div>
+                
+                <div class="col-2 texto_site justify-content-center">
+                           
+                
+
+                </div>
+          
+            </div>
+        </div>     
        
                  
-         
+<!-- GRIDS CONTAINERS NORMAIS MAIS DE UMA COLUNA basta adicionar mais cols detro de um container (Max=12) TAMANHOS DIFERENTES VIEWPORTS DIFERENTES-->
+<!--
+.col-		= MEDIDA EXTRA PEQUENA 	(Celulares)
+col-xl-4 	= MEDIDA EXTRA LARGA	(Televisores)
+col-sm-12 	= MEDIDA PEQUENA		(Celulares)
+col-lg-12	= MEDIDA LARGA			(Computadores)
+col-md-6	= MEDIDA MEDIA			(Tablets)
+
+-->
+        
+        <div class="container">
+        	<div class="row justify-content-center">
+            
+            	
+                <div class="col-xl-4 col-sm-12 texto_site justify-content-center">
+                           
+               
+                </div>
+                
+                <div class="col-xl-4 col-sm-12 texto_site justify-content-center">
+                           
+                
+
+                </div>
+                
+                <div class="col-xl-4 col-sm-12  texto_site justify-content-center">
+                           
+                
+
+                </div>
+          
+            </div>
+        </div>        
          
         
 <?php include_once "footer.php";?>
