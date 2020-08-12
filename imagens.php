0cbdc085d9be35d626a59df35d1cbaf5ada1bf48
Carlos Muniz <professorcarlosmuniz@gmail.com>
2020-08-11 21:31:50 -0300

    Add files via upload

---
+++ imagens.php
@@ -0,0 +1,109 @@
+<?php include_once "topo.php";?>
+        
+        
+<!-- IMAGENS-->
+<!-- OBS.: justify-content-center  SERVE PARA CENTRALIZAR NOP NAVEGADOR (Pode ser a esquerda, direita-->
+        
+        <div class="container">
+        	<div class="row justify-content-center">
+            
+            	
+                <div class="col">
+                
+                	<img src="imagens/bntp_aulas_de_informatica.fw.png" />
+                           
+                
+
+                </div>
+          
+            </div>
+        </div>
+        
+        
+        <!-- IMAGEM RESPONSIVA-->
+<!-- OBS.: justify-content-center  SERVE PARA CENTRALIZAR NOP NAVEGADOR (Pode ser a esquerda, direita-->
+        
+        <div class="container">
+        	<div class="row justify-content-center">
+            
+            	
+                <div class="col">
+                
+                	<img src="imagens/bntp_aulas_de_informatica.fw.png" class="img-fluid" />
+                           
+                
+
+                </div>
+          
+            </div>
+        </div>
+ 
+ 
+         <!-- IMAGEM DENTRO DE COLUNAS-->
+<!-- OBS.: justify-content-center  SERVE PARA CENTRALIZAR NOP NAVEGADOR (Pode ser a esquerda, direita-->
+        
+        <div class="container">
+        	<div class="row justify-content-center">
+            
+            	
+                <div class="col-3">
+                
+                	<img src="imagens/bntp_aulas_de_informatica.fw.png" class="img-fluid" />
+                           
+                
+
+                </div>
+          
+            </div>
+        </div>
+        
+        
+        
+<!-- IMAGEM THUMBNAIL (COM BORDAS E RESPONSIVA)->
+<!-- OBS.: justify-content-center  SERVE PARA CENTRALIZAR NOP NAVEGADOR (Pode ser a esquerda, direita-->
+        
+        <div class="container">
+        	<div class="row justify-content-center">
+            
+            	
+                <div class="col">
+                
+                	<img src="imagens/bntp_aulas_de_informatica.fw.png" class="img-thumbnail" />
+                           
+                
+
+                </div>
+          
+            </div>
+        </div>
+        
+        
+        
+        
+
+                 <!-- IMAGEM THUMBNAIL (COM BORDAS E RESPONSIVA e cantos arredondados)->
+<!-- OBS.: justify-content-center  SERVE PARA CENTRALIZAR NOP NAVEGADOR (Pode ser a esquerda, direita-->
+
+                 <!--É POSSIVEL FLOTAR IMAGENS-->
+        
+        <div class="container">
+        	<div class="row justify-content-center">
+            
+            	
+                <div class="col">
+                
+                	<img src="imagens/bntp_aulas_de_informatica.fw.png" class="img-thumbnail rounded" />
+                           
+                
+
+                </div>
+          
+            </div>
+        </div>
+        
+        
+        
+        
+
+        
+<?php include_once "footer.php";?>
