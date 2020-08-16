40c4b0e226e5808f736e516c1da4190b8e734684
Carlos Muniz <professorcarlosmuniz@gmail.com>
2020-08-16 14:24:01 -0300

    Add files via upload

---
+++ comp_alertas.php
@@ -0,0 +1,266 @@
+<?php include_once "topo.php";?>
+        
+        
+       
+
+        
+        <!-- ======================================================================================================= -->
+        
+         <!--ALERTAS-->
+         
+         <!-- ***CÓDIGOS DE ALERTA DENTRO DO CONTAINER***** -->
+        
+        <div class="container">
+        	<div class="row justify-content-center">
+            
+            	<div class="col-12">
+            
+            	                               
+                	<!--ALERTA 01-->
+                
+                	<div class="alert alert-primary" role="alert">
+                		Mensagem do alerta <a href="#" class="alert-link">Texto do link</a>, resto do texto...
+                	</div>
+                    
+                    
+                </div>
+            
+          </div>
+          
+          <!-- ***CÓDIGOS DE ALERTA DENTRO DO CONTAINER***** -->
+          
+          <div class="container">
+            
+          		<div class="row justify-content-center">
+            
+            			<div class="col-12">
+            
+            	                               
+                	
+                			<!--ALERTA 02-->
+                    
+                			<div class="alert alert-secondary" role="alert">
+                				Mensagem do alerta <a href="#" class="alert-link">Texto do link</a>, resto do texto...
+                			</div>
+                    
+                    
+                		</div>
+           
+            	</div>
+         	</div>
+            
+        
+        
+        
+        
+          <!-- ***CÓDIGOS DE ALERTA DENTRO DO CONTAINER***** -->
+          
+          <div class="container">
+            
+          		<div class="row justify-content-center">
+            
+            			<div class="col-12">
+            
+            	                               
+                	
+                			<!--ALERTA 03-->
+                    
+                			<div class="alert alert-success" role="alert">
+                				Mensagem do alerta <a href="#" class="alert-link">Texto do link</a>, resto do texto...
+                			</div>
+                    
+                    
+                		</div>
+           
+            	</div>
+         	</div>
+            
+            
+          <!-- ***CÓDIGOS DE ALERTA DENTRO DO CONTAINER***** -->
+          
+          <div class="container">
+            
+          		<div class="row justify-content-center">
+            
+            			<div class="col-12">
+            
+            	                               
+                	
+                			<!--ALERTA 04-->
+                    
+                			<div class="alert alert-danger" role="alert">
+                				Mensagem do alerta <a href="#" class="alert-link">Texto do link</a>, resto do texto...
+                			</div>
+                    
+                    
+                		</div>
+           
+            	</div>
+         	</div>
+            
+            
+
+          <!-- ***CÓDIGOS DE ALERTA DENTRO DO CONTAINER***** -->
+          
+          <div class="container">
+            
+          		<div class="row justify-content-center">
+            
+            			<div class="col-12">
+            
+            	                               
+                	
+                			<!--ALERTA 05-->
+                    
+                			<div class="alert alert-warning" role="alert">
+                				Mensagem do alerta <a href="#" class="alert-link">Texto do link</a>, resto do texto...
+                			</div>
+                    
+                    
+                		</div>
+           
+            	</div>
+         	</div>
+            
+            
+            
+          <!-- ***CÓDIGOS DE ALERTA DENTRO DO CONTAINER***** -->
+          
+          <div class="container">
+            
+          		<div class="row justify-content-center">
+            
+            			<div class="col-12">
+            
+            	                               
+                	
+                			<!--ALERTA 06-->
+                    
+                			<div class="alert alert-info" role="alert">
+                				Mensagem do alerta <a href="#" class="alert-link">Texto do link</a>, resto do texto...
+                			</div>
+                    
+                    
+                		</div>
+           
+            	</div>
+         	</div>
+            
+            
+          <!-- ***CÓDIGOS DE ALERTA DENTRO DO CONTAINER***** -->
+          
+          <div class="container">
+            
+          		<div class="row justify-content-center">
+            
+            			<div class="col-12">
+            
+            	                               
+                	
+                			<!--ALERTA 07-->
+                    
+                			<div class="alert alert-light" role="alert">
+                				Mensagem do alerta <a href="#" class="alert-link">Texto do link</a>, resto do texto...
+                			</div>
+                    
+                    
+                		</div>
+           
+            	</div>
+         	</div>
+            
+            
+                      <!-- ***CÓDIGOS DE ALERTA DENTRO DO CONTAINER***** -->
+          
+          <div class="container">
+            
+          		<div class="row justify-content-center">
+            
+            			<div class="col-12">
+            
+            	                               
+                	
+                			<!--ALERTA 08-->
+                    
+                			<div class="alert alert-dark" role="alert">
+                				Mensagem do alerta <a href="#" class="alert-link">Texto do link</a>, resto do texto...
+                			</div>
+                    
+                    
+                		</div>
+           
+            	</div>
+         	</div>
+            
+            
+            <div class="container">
+            
+          		<div class="row justify-content-center">
+            
+            			<div class="col-12">
+            
+            	                               
+                	
+                			<!--ALERTA COM TITULO-->
+                    
+                			<div class="alert alert-info" role="alert">
+                            
+                            	<h4 class="alert-heading"> Titulo do Alert!!!</h4>
+                                
+                				<p>
+                                
+                                Desfrute de todos os recursos oferecidos por este portal seja você meu aluno, ex-aluno, amigo ou um internauta. Fique a vontade para entrar em contato sempre quando quiser pois respondo a todos as mensagens. Procuro manter sempre o material gratuito, essa é a minha idéia!!! Portanto, para que eu possa continuar, não deixe de se inscrever no canal do you tube e curtir a minha página no facebook. Espero que possa ajudar a encontrar o que procura!!! Tenho como objetivo dividir um pouco do meu conhecimento com você, seja ou não aluno dos cursos ou instituições que leciono. Espero que goste do conteúdo e que volte sempre!!! Ahh!!! E não esqueça de conferir os cursos em vídeos em videoaulas!!! 
+                                
+                                
+                                </p>
+                                
+                                <hr />
+                                
+                                <p>
+                                
+                                Desfrute de todos os recursos oferecidos por este portal seja você meu aluno, ex-aluno, amigo ou um internauta. Fique a vontade para entrar em contato sempre quando quiser pois respondo a todos as mensagens. Procuro manter sempre o material gratuito, essa é a minha idéia!!! Portanto, para que eu possa continuar, não deixe de se inscrever no canal do you tube e curtir a minha página no facebook. Espero que possa ajudar a encontrar o que procura!!! Tenho como objetivo dividir um pouco do meu conhecimento com você, seja ou não aluno dos cursos ou instituições que leciono. Espero que goste do conteúdo e que volte sempre!!! Ahh!!! E não esqueça de conferir os cursos em vídeos em videoaulas!!! 
+                                
+                                
+                                </p>
+                                
+                			</div>
+                    
+                    
+                		</div>
+           
+            	</div>
+         	</div>
+            
+            
+            
+            
+            
+            
+             <div class="container">
+            
+          		<div class="row justify-content-center">
+            
+            			<div class="col-12">
+            
+            	                               
+                	
+                			<!--ALERTA COM BOTÕES-->
+                    
+                			<div class="alert alert-danger alert-dismissible fade show" role="alert">
+                            	<button type="button" class="close" data-dismiss="alert" aria-label="Fechar">
+                                	<span aria-hibben="true">&times;</span>
+                                </button>
+                                
+                				Mensagem do alerta <a href="#" class="alert-link">Texto do link</a>, resto do texto...
+                			</div>
+                    
+                    
+                		</div>
+           
+            	</div>
+         	</div>
+                 
+         
+         
+        
+<?php include_once "footer.php";?>
