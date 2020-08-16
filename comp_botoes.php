ab251a1c75dde26befaf04ea2bad1632ecf9dfed
Carlos Muniz <professorcarlosmuniz@gmail.com>
2020-08-16 15:49:32 -0300

    Add files via upload

---
+++ comp_botoes.php
@@ -0,0 +1,149 @@
+<?php include_once "topo.php";?>
+        
+        
+       
+
+        
+        <!-- ======================================================================================================= -->
+        
+         <!--BOTÕES
+         
+         Documentação: https://getbootstrap.com.br/docs/4.1/components/buttons/
+         
+         -->
+         
+         
+         <!-- ***CÓDIGOS DE BOTÕES DENTRO DO CONTAINER***** -->
+         
+         <div class="container">        
+        
+        
+        	<div class="row justify-content-center">
+            
+            	<div class="col-12">
+            
+            	                               
+                	<!--BOTÕES 01-->
+                
+                	<button class="btn btn-primary">Primário</button>
+                    <a href="#" class="btn btn-secondary" role="button">Secundario</a>
+                    <input type="button" class="btn btn-success" value="Success">
+                    <input type="submit" class="btn btn-danger" value="Danger">
+                    <input type="reset" class="btn btn-warning" value="Warning">
+                    
+                    <button class="btn btn-info">Informação</button>
+                    <button class="btn btn-light">Luz</button>
+                    <button class="btn btn-dark">Sombrio</button>
+                    <button class="btn btn-link">Link</button>
+                    
+                    
+                    
+                </div>
+            
+         
+          </div>
+          
+          
+                   <!-- ***CÓDIGOS DE BOTÕES DENTRO DO CONTAINER***** -->
+         
+         <div class="container">        
+        
+        
+        	<div class="row justify-content-center">
+            
+            	<div class="col-12">
+            
+            	                               
+                	<!--BOTÕES 01-->
+                
+                	<button class="btn btn-outline-primary">Primário</button>
+                    <a href="#" class="btn btn-outline-secondary" role="button">Secundario</a>
+                    <input type="button" class="btn btn-outline-success" value="Success">
+                    <input type="submit" class="btn btn-outline-danger" value="Danger">
+                    <input type="reset" class="btn btn-outline-warning" value="Warning">
+                    
+                    <button class="btn btn-outline-info">Informação</button>
+                    <button class="btn btn-outline-light">Luz</button>
+                    <button class="btn btn-outline-dark">Sombrio</button>
+                    <button class="btn btn-outline-link">Link</button>
+                    
+                    
+                    
+                </div>
+            
+         
+          </div>
+          
+
+                   <!-- ***CÓDIGOS DE BOTÕES DENTRO DO CONTAINER***** -->
+         
+         <div class="container">        
+        
+        
+        	<div class="row justify-content-center">
+            
+            	<div class="col-12">
+            
+            	                               
+                	<!--BOTÕES 01-->
+                
+                	<button class="btn btn-outline-primary btn-lg">Primário</button>
+                    <a href="#" class="btn btn-outline-secondary btn-sm" role="button">Secundario</a>
+                    
+                    
+                    
+                    
+                </div>
+            
+         
+          </div> 
+          
+                             <!-- ***CÓDIGOS DE BOTÕES DENTRO DO CONTAINER***** -->
+         
+         <div class="container">        
+        
+        
+        	<div class="row justify-content-center">
+            
+            	<div class="col-12">
+            
+            	                               
+                	<!--BOTÕES 01-->
+                
+                	<button class="btn btn-outline-primary btn-lg btn-block">Primário</button>
+                 
+                    
+                    
+                    
+                </div>
+            
+         
+          </div>
+          
+          
+          
+                   <!-- ***CÓDIGOS DE BOTÕES DENTRO DO CONTAINER***** -->
+         
+         <div class="container">        
+        
+        
+        	<div class="row justify-content-center">
+            
+            	<div class="col-12">
+            
+            	                               
+                	<!--BOTÕES 01-->
+                
+                	<button class="btn btn-outline-primary">Primário</button>
+                    <button class="btn btn-outline-primary active">Primário</button>
+                    <button class="btn btn-outline-primary" disabled="disabled">Primário</button>
+                    
+                    
+                    
+                    
+                </div>
+            
+         
+          </div>         
+ 
+<?php include_once "footer.php";?>
