59778fe06d06f80b21e632fd70304981dabd08bc
Carlos Muniz <professorcarlosmuniz@gmail.com>
2020-08-16 15:08:03 -0300

    Add files via upload

---
+++ comp_breadcumbs.php
@@ -0,0 +1,128 @@
+<?php include_once "topo.php";?>
+        
+        
+       
+
+        
+        <!-- ======================================================================================================= -->
+        
+         <!--BREADCRUMB
+         
+         Documentação: https://getbootstrap.com.br/docs/4.1/components/breadcrumb/
+         
+         -->
+         
+         
+         <!-- ***CÓDIGOS DE BREADCRUMB DENTRO DO CONTAINER***** -->
+         
+         <div class="container">        
+        
+        
+        	<div class="row justify-content-center">
+            
+            	<div class="col-12">
+            
+            	                               
+                	<!--BREADCUMB 01-->
+                
+                	<ol class="breadcrumb">
+                    
+                    	<li class="breadcrumb-item active">Home</li>
+                        
+                    
+                    
+                    </ol>
+                    
+                    
+                </div>
+            
+         
+          </div>
+          
+          
+         <!-- ***CÓDIGOS DE BREADCRUMB DENTRO DO CONTAINER***** -->
+         
+         <div class="container">        
+        
+        
+        	<div class="row justify-content-center">
+            
+            	<div class="col-12">
+            
+            	                               
+                	<!--BREADCUMB 01-->
+                
+                	<ol class="breadcrumb">
+                    
+                    	<li class="breadcrumb-item"><a href="#">Home</a></li>
+                        <li class="breadcrumb-item active"><a href="#">Biblioteca</a></li>
+                    
+                    
+                    </ol>
+                    
+                    
+                </div>
+            
+         
+          </div>
+        
+        
+        
+                 <!-- ***CÓDIGOS DE BREADCRUMB DENTRO DO CONTAINER***** -->
+         
+         <div class="container">        
+        
+        
+        	<div class="row justify-content-center">
+            
+            	<div class="col-12">
+            
+            	                               
+                	<!--BREADCUMB 01-->
+                
+                	<ol class="breadcrumb">
+                    
+                    	<li class="breadcrumb-item"><a href="#">Home</a></li>
+                        <li class="breadcrumb-item"><a href="#">Biblioteca</a></li>
+                        <li class="breadcrumb-item active"><a href="#">Item 3</a></li>
+                    
+                    
+                    </ol>
+                    
+                    
+                </div>
+            
+         
+          </div>
+          
+          
+          
+                 <!-- ***CÓDIGOS DE BREADCRUMB DENTRO DO CONTAINER***** -->
+         
+         <div class="container">        
+        
+        
+        	<div class="row justify-content-center">
+            
+            	<div class="col-12">
+            
+            	                               
+                	<!--BREADCUMB 01-->
+                
+                	<nav class="breadcrumb">
+                    
+                    	<a href class="breadcrumb-item"><a href="#">Home</a>
+                        <a href class="breadcrumb-item"="#">Biblioteca</a>
+                        <a href class="breadcrumb-item"="#">Imagens</a>
+                        
+                        <span class="breadcrumb-item active"> Menu </span>
+                    
+                    
+                    </nav>
+                    
+                    
+                </div>
+            
+         
+          </div>
+<?php include_once "footer.php";?>
