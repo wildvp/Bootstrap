893f170d3f1c7bcb8ac396e2fb3dfd4e269a9033
Carlos Muniz <professorcarlosmuniz@gmail.com>
2020-08-17 16:28:30 -0300

    Add files via upload

---
+++ comp_cards.php
@@ -0,0 +1,245 @@
+<?php include_once "topo.php";?>
+        
+        
+       
+
+        
+        <!-- ======================================================================================================= -->
+        
+         <!--CARDS
+         
+         Documentação: https://getbootstrap.com/docs/4.5/components/card/
+         
+         -->
+         
+         
+         <!-- ***CÓDIGOS DENTRO DO CONTAINER***** -->
+         
+         <!-- ***CARD SIMPLES***** -->
+         
+         <div class="container">        
+        
+        
+        	<div class="row justify-content-center">
+            
+            	<div class="col-12">
+                
+                	<div class="card">
+                    
+                    	<div class="card-body">
+                        
+                        	Texto do corpo do card
+                        
+                        </div>
+                    
+                    	
+                    
+                    </div>
+            
+            	                               
+                	
+                    
+                </div>
+            
+         
+         </div>
+          
+		<!-- ***FIM CODIGO***** -->
+        
+        
+        <!-- ***CARD TITULO SUBTITULO TEXTO***** -->
+         
+         <div class="container">        
+        
+        
+        	<div class="row justify-content-center">
+            
+            	<div class="col-12">
+                
+                	<div class="card" style="width:20rem;">
+                    
+                    	<div class="card-body">
+                        
+                        	<h4 class="card-title">Título do Card</h4>
+                            
+                            
+                            <h6 class="card-subtitle mb-2 text-muted">Subtitulo do Card</h6>
+                            
+                            <p class="card-text">
+                        
+                        	
+Desfrute de todos os recursos oferecidos por este portal seja você meu aluno, ex-aluno, amigo ou um internauta. Fique a vontade para entrar em contato sempre quando quiser pois respondo a todos as mensagens. Procuro manter sempre o material gratuito, essa é a minha idéia!!! Portanto, para que eu possa continuar, não deixe de se inscrever no canal do you tube e curtir a minha página no facebook. Espero que possa ajudar a encontrar o que procura!!! Tenho como objetivo dividir um pouco do meu conhecimento com você, seja ou não aluno dos cursos ou instituições que leciono. Espero que goste do conteúdo e que volte sempre!!! Ahh!!! E não esqueça de conferir os cursos em vídeos em videoaulas!!! 
+                            
+                            </p>
+                            
+                            <a href="#" class="card-link"> Link 1</a>
+                            <a href="#" class="card-link"> Link 2</a>
+                        
+                        </div>
+                    
+                    	
+                    
+                    </div>
+            
+            	                               
+                	
+                    
+                </div>
+            
+         
+         </div>
+          
+		<!-- ***FIM CODIGO***** -->
+        
+        
+         <!-- ***CARD TITULO SUBTITULO TEXTO IMAGEM***** -->
+         
+         <div class="container">        
+        
+        
+        	<div class="row justify-content-center">
+            
+            	<div class="col-12">
+                
+                	<div class="card" style="width:20rem;">
+                    
+                    	<img src="imagens/bntp_monografias.fw.png" class="card-img-top" />
+                    
+                    	<div class="card-body">
+                        
+                        	<h4 class="card-title">Título do Card</h4>
+                            
+                            
+                            <h6 class="card-subtitle mb-2 text-muted">Subtitulo do Card</h6>
+                            
+                            <p class="card-text">
+                        
+                        	
+Desfrute de todos os recursos oferecidos por este portal seja você meu aluno, ex-aluno, amigo ou um internauta. Fique a vontade para entrar em contato sempre quando quiser pois respondo a todos as mensagens. Procuro manter sempre o material gratuito, essa é a minha idéia!!! Portanto, para que eu possa continuar, não deixe de se inscrever no canal do you tube e curtir a minha página no facebook. Espero que possa ajudar a encontrar o que procura!!! Tenho como objetivo dividir um pouco do meu conhecimento com você, seja ou não aluno dos cursos ou instituições que leciono. Espero que goste do conteúdo e que volte sempre!!! Ahh!!! E não esqueça de conferir os cursos em vídeos em videoaulas!!! 
+                            
+                            </p>
+                            
+                            <a href="#" class="card-link"> Link 1</a>
+                            <a href="#" class="card-link"> Link 2</a>
+                        
+                        </div>
+                    
+                    	
+                    
+                    </div>
+            
+            	                               
+                	
+                    
+                </div>
+            
+         
+         </div>
+          
+		<!-- ***FIM CODIGO***** -->
+        
+                 <!-- ***CARD  LISTAS***** -->
+         
+         <div class="container">        
+        
+        
+        	<div class="row justify-content-center">
+            
+            	<div class="col-12">
+                
+                	<div class="card" style="width:20rem;">
+                    
+                    	
+                        <ul class="list-group list-group-flush">
+                        
+                        	<li class="list-group-item">Item 1</li>
+                            <li class="list-group-item">Item 2</li>
+                            <li class="list-group-item">Item 3</li>
+                            <li class="list-group-item">Item 4</li>
+                            <li class="list-group-item">Item 5</li>
+                        
+                        </ul>
+                       
+                    	
+                    
+                    </div>
+            
+            	                               
+                	
+                    
+                </div>
+            
+         
+         </div>
+          
+		<!-- ***FIM CODIGO***** -->
+        
+        
+        
+                 <!-- ***CARD TITULO SUBTITULO TEXTO IMAGEM***** -->
+         
+         <div class="container">        
+        
+        
+        	<div class="row justify-content-center">
+            
+            	<div class="col-12">
+                
+                	<div class="card" style="width:20rem;">
+                    
+                    	<img src="imagens/bntp_monografias.fw.png" class="card-img-top" />
+                    
+                    	<div class="card-body">
+                        
+                        	<h4 class="card-title">Título do Card</h4>
+                            
+                            
+                            <h6 class="card-subtitle mb-2 text-muted">Subtitulo do Card</h6>
+                            
+                            <p class="card-text">
+                        
+                        	
+Desfrute de todos os recursos oferecidos por este portal seja você meu aluno, ex-aluno, amigo ou um internauta. Fique a vontade para entrar em contato sempre quando quiser pois respondo a todos as mensagens. Procuro manter sempre o material gratuito, essa é a minha idéia!!! Portanto, para que eu possa continuar, não deixe de se inscrever no canal do you tube e curtir a minha página no facebook. Espero que possa ajudar a encontrar o que procura!!! Tenho como objetivo dividir um pouco do meu conhecimento com você, seja ou não aluno dos cursos ou instituições que leciono. Espero que goste do conteúdo e que volte sempre!!! Ahh!!! E não esqueça de conferir os cursos em vídeos em videoaulas!!! 
+                            
+                            </p>
+                            
+                          </div>
+                          
+                          <ul class="list-group list-group-flush">
+                        
+                        	<li class="list-group-item">Item 1</li>
+                            <li class="list-group-item">Item 2</li>
+                            <li class="list-group-item">Item 3</li>
+                            <li class="list-group-item">Item 4</li>
+                            <li class="list-group-item">Item 5</li>
+                        
+                        </ul>
+                          
+                          <div class="card-body">
+                            
+                            <a href="#" class="card-link"> Link 1</a>
+                            <a href="#" class="card-link"> Link 2</a>
+                            
+                          </div>
+                        
+                        </div>
+                    
+                    	
+                    
+                    </div>
+            
+            	                               
+                	
+                    
+                </div>
+            
+         
+         </div>
+          
+		<!-- ***FIM CODIGO***** -->
+        
+        
+        
+                  
+         
+<?php include_once "footer.php";?>
