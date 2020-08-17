ea5f0e38907f82ada08909bd8bbe6952b405229b
Carlos Muniz <professorcarlosmuniz@gmail.com>
2020-08-16 21:14:15 -0300

    Add files via upload

---
+++ comp_grp_botoes.php
@@ -0,0 +1,184 @@
+<?php include_once "topo.php";?>
+        
+        
+       
+
+        
+        <!-- ======================================================================================================= -->
+        
+         <!--GRUPO DE BOTÕES
+         
+         Documentação: https://getbootstrap.com.br/docs/4.1/components/button-group/
+         
+         -->
+         
+         
+         <!-- ***CÓDIGOS DE BOTÕES DENTRO DO CONTAINER***** -->
+         
+         <div class="container">        
+        
+        
+        	<div class="row justify-content-center">
+            
+            	<div class="col-12">
+                
+                	<div class="btn-group" role="group" aria-label="Exemplo_1">
+                    
+                    	<button class="btn btn-primary">Botão 1</button>
+                        <button class="btn btn-primary">Botão 2</button>
+                        <button class="btn btn-primary">Botão 3</button>
+                    
+                    </div>
+            
+            	                               
+                	
+                    
+                </div>
+            
+         
+          </div>
+          
+
+
+<!-- ***CÓDIGOS DE BOTÕES DENTRO DO CONTAINER TOOLBAR HORIZONTAL***** -->          
+         <div class="container">        
+        
+        
+        	<div class="row justify-content-center">
+            
+            	<div class="col-12">
+                
+                	<div class="btn-toolbar" role="toolbar" aria-label="Toobar de Botoes">
+                    
+                    	<div class="btn-group mr-2" role="group" aria-label="Primeiro Grupo">
+                    
+                    		<button class="btn btn-secundary">Botão 1</button>
+                        	<button class="btn btn-secundary">Botão 2</button>
+                        	<button class="btn btn-secundary">Botão 3</button>
+                    
+                   		</div>
+                        
+                        <div class="btn-group mr-2" role="group" aria-label="Segundo Grupo">
+                    
+                    		<button class="btn btn-secundary">Botão 4</button>
+                        	<button class="btn btn-secundary">Botão 5</button>
+                        	<button class="btn btn-secundary">Botão 6</button>
+                    
+                   		</div>
+                        
+                        <div class="btn-group mr-2" role="group" aria-label="Terceiro Grupo">
+                    
+                    		<button class="btn btn-secundary">Botão 6</button>
+                        	<button class="btn btn-secundary">Botão 7</button>
+                        	<button class="btn btn-secundary">Botão 8</button>
+                    
+                   		</div>
+                    
+                    </div>
+                
+                	
+            
+            	                               
+                	
+                    
+                </div>
+            
+         
+          </div>
+          
+          
+          <!-- ***CÓDIGOS DE BOTÕES DENTRO DO CONTAINER TOOLBAR HORIZONTAL***** -->          
+         <div class="container">        
+        
+        
+        	<div class="row justify-content-center">
+            
+            	<div class="col-12">
+                
+                	<div class="btn-toolbar" role="toolbar" aria-label="Toobar de Botoes">
+                    
+                    	<div class="btn-group mr-2 btn-group-lg" role="group" aria-label="Primeiro Grupo">
+                    
+                    		<button class="btn btn-secundary">Botão 1</button>
+                        	<button class="btn btn-secundary">Botão 2</button>
+                        	<button class="btn btn-secundary">Botão 3</button>
+                    
+                   		</div>
+                        
+                        <div class="btn-group mr-2 btn-group-sm" role="group" aria-label="Segundo Grupo">
+                    
+                    		<button class="btn btn-secundary">Botão 4</button>
+                        	<button class="btn btn-secundary">Botão 5</button>
+                        	<button class="btn btn-secundary">Botão 6</button>
+                    
+                   		</div>
+                        
+                        <div class="btn-group mr-2 btn-group-lg" role="group" aria-label="Terceiro Grupo">
+                    
+                    		<button class="btn btn-secundary">Botão 6</button>
+                        	<button class="btn btn-secundary">Botão 7</button>
+                        	<button class="btn btn-secundary">Botão 8</button>
+                    
+                   		</div>
+                    
+                    </div>
+                
+                	
+            
+            	                               
+                	
+                    
+                </div>
+            
+         
+          </div>              
+ 
+ 
+ 
+           <!-- ***CÓDIGOS DE BOTÕES DENTRO DO CONTAINER TOOLBAR VERTICAL***** -->          
+         <div class="container">        
+        
+        
+        	<div class="row justify-content-center">
+            
+            	<div class="col-12">
+                
+                	<div class="btn-toolbar" role="toolbar" aria-label="Toobar de Botoes">
+                    
+                    	<div class="btn-group-vertical mr-2 btn-group-lg" role="group" aria-label="Primeiro Grupo">
+                    
+                    		<button class="btn btn-secundary">Botão 1</button>
+                        	<button class="btn btn-secundary">Botão 2</button>
+                        	<button class="btn btn-secundary">Botão 3</button>
+                    
+                   		</div>
+                        
+                        <div class="btn-group-vertical mr-2 btn-group-sm" role="group" aria-label="Segundo Grupo">
+                    
+                    		<button class="btn btn-secundary">Botão 4</button>
+                        	<button class="btn btn-secundary">Botão 5</button>
+                        	<button class="btn btn-secundary">Botão 6</button>
+                    
+                   		</div>
+                        
+                        <div class="btn-group-vertical mr-2 btn-group-lg" role="group" aria-label="Terceiro Grupo">
+                    
+                    		<button class="btn btn-secundary">Botão 6</button>
+                        	<button class="btn btn-secundary">Botão 7</button>
+                        	<button class="btn btn-secundary">Botão 8</button>
+                    
+                   		</div>
+                    
+                    </div>
+                
+                	
+            
+            	                               
+                	
+                    
+                </div>
+            
+         
+          </div>              
+ 
+<?php include_once "footer.php";?>
