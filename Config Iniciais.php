b30d20d0de60d49beff9ab71003f54578dacb795
Carlos Muniz <professorcarlosmuniz@gmail.com>
2020-08-30 17:32:21 -0300

    Add files via upload

---
+++ Config Iniciais.php
@@ -0,0 +1,58 @@
+<?php
+
+	// Chamando a página topo
+
+?>
+
+<!--CONCIFURAÇÕES INICIAIS -->
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+
+	<charset="utf-8">
+	<meta name="viewport" content="width-device-width, initial-sacale = 1, shrink-to-fit=no">
+	<title>Configurações Iniciais</title>
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+
+</head>
+
+<body>
+
+
+	<h1> Olá Mundo!!! </h1>
+
+
+
+
+
+
+	<!-- PLUGINS BOOTSTRAP-->
+    <!-- JQuery -->
+    <script src="js/jquery-3.5.1.min.js"></script>
+    <!-- Popper -->
+    <script src="js/popper.min.js"></script>
+    <!-- Javascript -->
+    <script src="js/bootstrap.min.js"></script>
+	
+   
+
+
+</body>
+
+</html>
+
+
+
+
+
+
+
+
+
+
+
+<?php
+
+	// Chamando a página rodapé
+
+?>
