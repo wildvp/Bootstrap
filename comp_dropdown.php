8b0a0162bbaf351e55ea4d75a92c1cda18d84bb3
Carlos Muniz <professorcarlosmuniz@gmail.com>
2020-09-12 11:17:24 -0300

    Add files via upload

---
+++ comp_dropdown.php
@@ -0,0 +1,269 @@
+<?php
+
+	include_once "topo.php";
+
+?>
+
+
+	<div class="container">
+    
+    	<div class="row">
+        
+        	<div class="col-12">
+                        
+            	<!--DROPDOWN -->
+                <!-- EM UM BOTÃO -->
+                
+                
+                <div class="dropdown">
+                
+                	<button id="button1" class="btn btn-primary dropdown-toggle" type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"> Dropodown</button>
+                    
+                    <!--ITENS DO DROPDOWN -->
+                    
+                    <div class="dropdown-menu" aria-labelledby="button1">
+                    
+                    	<a class="dropdown-item" href="#">Link 1</a>
+                        <a class="dropdown-item" href="#">Link 2</a>
+                        <a class="dropdown-item" href="#">Link 3</a>
+                    
+                    
+                    </div>
+                
+                </div>
+                	
+                
+                
+             </div>
+        
+        </div>
+    
+    </div>
+    
+    
+    
+    
+    
+    <div class="container">
+    
+    	<div class="row">
+        
+        	<div class="col-12">
+                        
+            	<!--DROPDOWN -->
+                <!-- EM UM LINK -->
+                
+                
+                <div class="dropdown">
+                
+                	<a id="button2" class="btn btn-primary dropdown-toggle" href="#" role="button" data-aria-haspopup="true" data-toggle="dropdown" aria-expanded="false">Link Dropdown</a>
+                
+                	
+                    
+                    <!--ITENS DO DROPDOWN -->
+                    
+                    <div class="dropdown-menu" aria-labelledby="button2">
+                    
+                    	<a class="dropdown-item" href="#">Link 1</a>
+                        <a class="dropdown-item" href="#">Link 2</a>
+                        <a class="dropdown-item" href="#">Link 3</a>
+                    
+                    
+                    </div>
+                
+                </div>
+                	
+                
+                
+             </div>
+        
+        </div>
+    
+    </div>
+    
+    
+    
+    
+        <div class="container">
+    
+    	<div class="row">
+        
+        	<div class="col-12">
+                        
+            	<!--DROPDOWN -->
+                <!-- EM UM LINK  -->
+                
+                
+                <div class="dropdown">
+                
+                	<a id="button3" class="btn btn-primary dropdown-toggle" href="#" role="button" data-aria-haspopup="true" data-toggle="dropdown" aria-expanded="false">Link Dropdown</a>
+                
+                	
+                    
+                    <!--ITENS DO DROPDOWN (COM DIVISÕES)-->
+                    
+                    <div class="dropdown-menu" aria-labelledby="button3">
+                    
+                    	<a class="dropdown-item" href="#">Link 1</a>
+                        <a class="dropdown-item" href="#">Link 2</a>
+                        <a class="dropdown-item" href="#">Link 3</a>
+                        <div class="dropdown-divider"></div>
+                        <a class="dropdown-item" href="#">Link 4</a>
+                        <a class="dropdown-item" href="#">Link 5</a>
+                        <a class="dropdown-item" href="#">Link 6</a>
+                    
+                    
+                    </div>
+                
+                </div>
+                	
+                
+                
+             </div>
+        
+        </div>
+    
+    </div>
+    
+    
+    
+    <div class="container">
+    
+    	<div class="row">
+        
+        	<div class="col-12">
+                        
+            	<!--DROPDOWN -->
+                <!-- EM UM LINK  -->
+                
+                
+                <div class="dropdown">
+                
+                	<a id="button4" class="btn btn-primary dropdown-toggle" href="#" role="button" data-aria-haspopup="true" data-toggle="dropdown" aria-expanded="false">Link Dropdown</a>
+                
+                	
+                    
+                    <!--ITENS DO DROPDOWN (COM DIVISÕES) - MENU COM CABEÇALHO-->
+                    
+                    <div class="dropdown-menu" aria-labelledby="button4">
+                    
+                    	<h6 class="dropdown-header"> Cabeçalho</h6>
+                    
+                    	<a class="dropdown-item" href="#">Link 1</a>
+                        <a class="dropdown-item" href="#">Link 2</a>
+                        <a class="dropdown-item" href="#">Link 3</a>
+                        <div class="dropdown-divider"></div>
+                        <a class="dropdown-item" href="#">Link 4</a>
+                        <a class="dropdown-item" href="#">Link 5</a>
+                        <a class="dropdown-item" href="#">Link 6</a>
+                    
+                    
+                    </div>
+                
+                </div>
+                	
+                
+                
+             </div>
+        
+        </div>
+    
+    </div>
+    
+    
+    
+        <div class="container">
+    
+    	<div class="row">
+        
+        	<div class="col-12">
+                        
+            	<!--DROPDOWN -->
+                <!-- EM UM LINK  -->
+                
+                
+                <div class="dropdown">
+                
+                	<a id="button4" class="btn btn-primary dropdown-toggle" href="#" role="button" data-aria-haspopup="true" data-toggle="dropdown" aria-expanded="false">Link Dropdown</a>
+                
+                	
+                    
+                    <!--ITENS DO DROPDOWN (COM DIVISÕES) - MENU COM DOIS CABEÇALHOS-->
+                    
+                    <div class="dropdown-menu" aria-labelledby="button4">
+                    
+                    	<h6 class="dropdown-header"> Cabeçalho</h6>
+                    
+                    	<a class="dropdown-item" href="#">Link 1</a>
+                        <a class="dropdown-item" href="#">Link 2</a>
+                        <a class="dropdown-item" href="#">Link 3</a>
+                        <div class="dropdown-divider"></div>
+                        
+                        <h6 class="dropdown-header"> Cabeçalho 2</h6>
+                        <a class="dropdown-item" href="#">Link 4</a>
+                        <a class="dropdown-item" href="#">Link 5</a>
+                        <a class="dropdown-item" href="#">Link 6</a>
+                    
+                    
+                    </div>
+                
+                </div>
+                	
+                
+                
+             </div>
+        
+        </div>
+    
+    </div>
+    
+            <div class="container">
+    
+    	<div class="row">
+        
+        	<div class="col-12">
+                        
+            	<!--DROPDOWN -->
+                <!-- EM UM LINK  -->
+                
+                
+                <div class="dropdown">
+                
+                	<a id="button5" class="btn btn-primary dropdown-toggle" href="#" role="button" data-aria-haspopup="true" data-toggle="dropdown" aria-expanded="false">Link Dropdown</a>
+                
+                	
+                    
+                    <!--ITENS DO DROPDOWN (COM DIVISÕES) - MENU COM DOIS CABEÇALHOS COM LINKS DESABILITADOS-->
+                    
+                    <div class="dropdown-menu" aria-labelledby="button5">
+                    
+                    	<h6 class="dropdown-header"> Cabeçalho</h6>
+                    
+                    	<a class="dropdown-item" href="#">Link 1</a>
+                        <a class="dropdown-item disabled" href="#">Link 2</a>
+                        <a class="dropdown-item" href="#">Link 3</a>
+                        <div class="dropdown-divider"></div>
+                        
+                        <h6 class="dropdown-header"> Cabeçalho 2</h6>
+                        <a class="dropdown-item" href="#">Link 4</a>
+                        <a class="dropdown-item" href="#">Link 5</a>
+                        <a class="dropdown-item" href="#">Link 6</a>
+                    
+                    
+                    </div>
+                
+                </div>
+                	
+                
+                
+             </div>
+        
+        </div>
+    
+    </div>
+
+<?php
+
+	include_once "footer.php";
+
+?>
