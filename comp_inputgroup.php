951d88cf1d2ce86e940421aa42e12677d1370603
Carlos Muniz <professorcarlosmuniz@gmail.com>
2020-09-17 10:32:40 -0300

    Add files via upload

---
+++ comp_inputgroup.php
@@ -0,0 +1,114 @@
+<?php
+
+	include_once "topo.php";
+
+?>
+
+<!-- INPUT GROUP -->
+
+
+<div class="container">
+    
+    <div class="row">
+    
+    	<div class="col-12">
+                
+        	<!-- INPUT GROUP -->
+            
+        	<div class="input-group">
+            
+            	<span>@</span>
+                <input type="text" name="input1">              
+                        
+            </div>
+        
+        </div>
+    
+    </div>
+    
+    <div class="row">
+    
+    	<div class="col-4">
+                
+        	<!-- INPUT GROUP -->
+            
+        	<div class="input-group">
+            
+            	<span class="input-group-addon" id="input2"> @ </span>
+                <input type="text" class="form-control" name="input2" placeholder="Nome de Usuário" aria-label="Nome de Usuário" aria-describedby="input2">              
+                        
+            </div>
+        
+        </div>
+    
+    </div>
+    
+    
+    
+    <div class="row">
+    
+    	<div class="col-4">
+                
+        	<!-- INPUT GROUP -->
+            
+        	<div class="input-group">
+            
+            	
+                <input type="text" class="form-control" name="input2" placeholder="Nome de Usuário" aria-label="Nome de Usuário" aria-describedby="input2">
+                <span class="input-group-addon" id="input2"> @seu dominio.com.br </span>             
+                        
+            </div>
+        
+        </div>
+    
+    </div>
+    
+    
+    
+    <div class="row">
+    
+    	<div class="col-4">
+                
+        	<!-- INPUT GROUP -->
+            
+        	<div class="input-group">
+            
+            	<span class="input-group-addon" id="input2"> Login </span>
+                <input type="text" class="form-control" name="input2" placeholder="Nome de Usuário" aria-label="Nome de Usuário" aria-describedby="input2">
+                <span class="input-group-addon" id="input2"> @seu dominio.com.br </span>             
+                        
+            </div>
+        
+        </div>
+    
+    </div>
+    
+    
+    <div class="row">
+    
+    	<div class="col-4">
+                
+        	<!-- INPUT GROUP -->
+            
+        	<div class="input-group">
+            
+            	<span class="input-group-addon" id="input2"> Login </span>
+                <input type="text" class="form-control" name="input2" placeholder="Nome de Usuário" aria-label="Nome de Usuário" aria-describedby="input2">
+                <span class="input-group-addon" id="input2"> @seu dominio.com.br </span>             
+                        
+            </div>
+        
+        </div>
+    
+    </div>
+        
+</div>
+  
+  
+  
+
+<?php
+
+	include_once "footer.php";
+
+?>
