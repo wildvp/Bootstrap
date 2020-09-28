1eda8d578e2f1bc3277e7e7662e59fa37c81139b
Carlos Muniz <professorcarlosmuniz@gmail.com>
2020-09-28 11:19:44 -0300

    Add files via upload

---
+++ comp_paginador.php
@@ -0,0 +1,270 @@
+<?php
+
+	include_once "topo.php";
+
+?>
+
+<!-- PAGINADOR -->
+
+
+<div class="container">
+    
+    <div class="row">
+    
+    	<div class="col-12">
+                
+        	<!-- PAGINADOR -->
+            
+            <nav aria-label="Paginador">
+            
+            	<ul class="pagination">
+                
+                	<li class="page-item"><a class="page-link" href="#">Anterior</a></li>
+                	<li class="page-item"><a class="page-link" href="#">1</a></li>
+                    <li class="page-item"><a class="page-link" href="#">2</a></li>
+                    <li class="page-item"><a class="page-link" href="#">3</a></li>
+                    <li class="page-item"><a class="page-link" href="#">Próximo</a></li>
+                
+                </ul>
+            
+            </nav>
+                    	
+        </div>
+    
+    </div>
+        
+</div>
+
+
+
+
+
+
+
+
+<div class="container">
+    
+    <div class="row">
+    
+    	<div class="col-12">
+                
+        	<!-- PAGINADOR ÍCONES-->
+            
+            <nav aria-label="Paginador">
+            
+            	<ul class="pagination">
+                
+                	<li class="page-item">
+                    	<a class="page-link" href="#">
+                        
+                        	<span aria-hidden="true">&laquo;</span>
+                            <span class="sr-only">Anterior</span>
+                        
+                    	</a>
+                    </li>
+                    
+                	<li class="page-item"><a class="page-link" href="#">1</a></li>
+                    <li class="page-item"><a class="page-link" href="#">2</a></li>
+                    <li class="page-item"><a class="page-link" href="#">3</a></li>
+                    
+                    <li class="page-item">
+                    	<a class="page-link" href="#">
+                        
+                        	<span aria-hidden="true">&raquo;</span>
+                            <span class="sr-only">Próximo</span>
+                        
+                    	</a>
+                    </li>
+                
+                </ul>
+            
+            </nav>
+                    	
+        </div>
+    
+    </div>
+        
+</div>
+
+
+
+
+
+
+
+
+<!-- PAGINADOR -->
+
+
+<div class="container">
+    
+    <div class="row">
+    
+    	<div class="col-12">
+                
+        	<!-- PAGINADOR -->
+            
+            <nav aria-label="Paginador">
+            
+            	<ul class="pagination">
+                
+                	<li class="page-item disabled"><a class="page-link" href="#">Anterior</a></li>
+                	<li class="page-item"><a class="page-link" href="#">1</a></li>
+                    <li class="page-item active"><a class="page-link" href="#">2</a></li>
+                    <li class="page-item"><a class="page-link" href="#">3</a></li>
+                    <li class="page-item"><a class="page-link" href="#">Próximo</a></li>
+                
+                </ul>
+            
+            </nav>
+                    	
+        </div>
+    
+    </div>
+        
+</div>
+
+
+
+
+
+
+<!-- PAGINADOR -->
+
+
+<div class="container">
+    
+    <div class="row">
+    
+    	<div class="col-12">
+                
+        	<!-- PAGINADOR GRANDE-->
+            
+            <nav aria-label="Paginador">
+            
+            	<ul class="pagination pagination-lg">
+                
+                	<li class="page-item disabled"><a class="page-link" href="#">Anterior</a></li>
+                	<li class="page-item"><a class="page-link" href="#">1</a></li>
+                    <li class="page-item active"><a class="page-link" href="#">2</a></li>
+                    <li class="page-item"><a class="page-link" href="#">3</a></li>
+                    <li class="page-item"><a class="page-link" href="#">Próximo</a></li>
+                
+                </ul>
+            
+            </nav>
+                    	
+        </div>
+    
+    </div>
+        
+</div>
+
+
+
+
+
+
+<!-- PAGINADOR PEQUENO-->
+
+
+<div class="container">
+    
+    <div class="row">
+    
+    	<div class="col-12">
+                
+        	<!-- PAGINADOR -->
+            
+            <nav aria-label="Paginador">
+            
+            	<ul class="pagination pagination-sm">
+                
+                	<li class="page-item disabled"><a class="page-link" href="#">Anterior</a></li>
+                	<li class="page-item"><a class="page-link" href="#">1</a></li>
+                    <li class="page-item active"><a class="page-link" href="#">2</a></li>
+                    <li class="page-item"><a class="page-link" href="#">3</a></li>
+                    <li class="page-item"><a class="page-link" href="#">Próximo</a></li>
+                
+                </ul>
+            
+            </nav>
+                    	
+        </div>
+    
+    </div>
+        
+</div>
+
+
+<!-- PAGINADOR CENTRO-->
+
+
+<div class="container">
+    
+    <div class="row">
+    
+    	<div class="col-12">
+                
+        	<!-- PAGINADOR -->
+            
+            <nav aria-label="Paginador">
+            
+            	<ul class="pagination justify-content-center">
+                
+                	<li class="page-item disabled"><a class="page-link" href="#">Anterior</a></li>
+                	<li class="page-item"><a class="page-link" href="#">1</a></li>
+                    <li class="page-item active"><a class="page-link" href="#">2</a></li>
+                    <li class="page-item"><a class="page-link" href="#">3</a></li>
+                    <li class="page-item"><a class="page-link" href="#">Próximo</a></li>
+                
+                </ul>
+            
+            </nav>
+                    	
+        </div>
+    
+    </div>
+        
+</div>
+
+
+<!-- PAGINADOR FIM-->
+
+
+<div class="container">
+    
+    <div class="row">
+    
+    	<div class="col-12">
+                
+        	<!-- PAGINADOR -->
+            
+            <nav aria-label="Paginador">
+            
+            	<ul class="pagination justify-content-end">
+                
+                	<li class="page-item disabled"><a class="page-link" href="#">Anterior</a></li>
+                	<li class="page-item"><a class="page-link" href="#">1</a></li>
+                    <li class="page-item active"><a class="page-link" href="#">2</a></li>
+                    <li class="page-item"><a class="page-link" href="#">3</a></li>
+                    <li class="page-item"><a class="page-link" href="#">Próximo</a></li>
+                
+                </ul>
+            
+            </nav>
+                    	
+        </div>
+    
+    </div>
+        
+</div>
+  
+  
+  
+
+<?php
+
+	include_once "footer.php";
+
+?>
