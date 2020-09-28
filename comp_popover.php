14665c111a5ff2f8ce4f2087d57e43019fa78ad1
Carlos Muniz <professorcarlosmuniz@gmail.com>
2020-09-28 12:54:06 -0300

    Add files via upload

---
+++ comp_popover.php
@@ -0,0 +1,63 @@
+<?php
+
+	include_once "topo.php";
+
+?>
+
+<!-- POPPOVER -->
+
+
+<div class="container">
+    
+    <div class="row">
+    
+    	<div class="col-12">
+                
+        	<!-- POPOVER -->
+            <!--
+            Obs. Para hyabilitar o popover, há um javascript no rodapé
+            
+            -->
+            
+            <button type="button" class="btn btn-lg btn-danger" data-toggle="popover" title="Titulo do Popover" data-content="Desfrute de todos os recursos oferecidos por este portal seja você meu aluno, ex-aluno, amigo ou um internauta. Fique a vontade para entrar em contato sempre quando quiser pois respondo a todos as mensagens. Procuro manter sempre o material gratuito, essa é a minha idéia!!! Portanto, para que eu possa continuar, não deixe de se inscrever no canal do you tube e curtir a minha página no facebook. Espero que possa ajudar a encontrar o que procura!!! Tenho como objetivo dividir um pouco do meu conhecimento com você, seja ou não aluno dos cursos ou instituições que leciono. Espero que goste do conteúdo e que volte sempre!!! Ahh!!! E não esqueça de conferir os cursos em vídeos em videoaulas!!!">
+            
+            Clique para acionar o popover
+            
+            </button>
+            
+            
+                    	
+        </div>
+    
+    </div>
+    
+    
+    
+    
+    <!-- POPOVER DIRECIONADO ACIMA, AO LADO OU ABAIXO DO BOTÃO-->
+    
+    
+    <div class="row">
+    
+    	<div class="col-12">
+        	
+            <button type="button" class="btn btn-secondary" data-container="body" data-toggle="popover" data-placement="top" title="Titulo do Popover" data-align="justify" data-content="Desfrute de todos os recursos oferecidos por este portal seja você meu aluno, ex-aluno, amigo ou um internauta. Fique a vontade para entrar em contato sempre quando quiser pois respondo a todos as mensagens. Procuro manter sempre o material gratuito, essa é a minha idéia!!! Portanto, para que eu possa continuar, não deixe de se inscrever no canal do you tube e curtir a minha página no facebook. Espero que possa ajudar a encontrar o que procura!!! Tenho como objetivo dividir um pouco do meu conhecimento com você, seja ou não aluno dos cursos ou instituições que leciono. Espero que goste do conteúdo e que volte sempre!!! Ahh!!! E não esqueça de conferir os cursos em vídeos em videoaulas!!!">
+            
+            Popover - Topo
+            
+            </button>
+        
+    	</div>
+        
+    </div>
+        
+</div>
+
+
+
+
+<?php
+
+	include_once "footer.php";
+
+?>
