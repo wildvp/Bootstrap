4a4c91984c2a6cda1508dcd93a1136d9c9504982
Carlos Muniz <professorcarlosmuniz@gmail.com>
2020-09-29 17:40:47 -0300

    Add files via upload

---
+++ comp_listgroup_2.php
@@ -0,0 +1,141 @@
+<?php
+
+	include_once "topo.php";
+
+?>
+
+<!-- LISTGROUP -->
+
+
+<div class="container">
+    
+    <div class="row">
+    
+    	<div class="col-4">
+        
+        	<div class="list-group">
+            
+            	<a href="#" class="list-group-item list-group-item-action active">
+            
+            		<div class="d-flex w-100 justify-content-between">
+                		<h5>Titulo da lista</h5>
+                    	<small>Tres dias atrás</small>
+                    </div>
+                    
+                	<p class="mb-1">Desfrute de todos os recursos oferecidos por este portal seja você meu aluno, ex-aluno, amigo ou um internauta. Fique a vontade para entrar em contato sempre quando quiser pois respondo a todos as mensagens. Procuro manter sempre o material gratuito, essa é a minha idéia!!! Portanto, para que eu possa continuar, não deixe de se inscrever no canal do you tube e curtir a minha página no facebook. Espero que possa ajudar a encontrar o que procura!!! Tenho como objetivo dividir um pouco do meu conhecimento com você, seja ou não aluno dos cursos ou instituições que leciono. Espero que goste do conteúdo e que volte sempre!!! Ahh!!! E não esqueça de conferir os cursos em vídeos em videoaulas!!! </p>
+                    
+                </a>
+                
+                <a href="#" class="list-group-item list-group-item-action">
+            
+            		<div class="d-flex w-100 justify-content-between">
+                		<h5>Titulo da lista</h5>
+                    	<small>Tres dias atrás</small>
+                    </div>
+                    
+                	<p class="mb-1">Desfrute de todos os recursos oferecidos por este portal seja você meu aluno, ex-aluno, amigo ou um internauta. Fique a vontade para entrar em contato sempre quando quiser pois respondo a todos as mensagens. Procuro manter sempre o material gratuito, essa é a minha idéia!!! Portanto, para que eu possa continuar, não deixe de se inscrever no canal do you tube e curtir a minha página no facebook. Espero que possa ajudar a encontrar o que procura!!! Tenho como objetivo dividir um pouco do meu conhecimento com você, seja ou não aluno dos cursos ou instituições que leciono. Espero que goste do conteúdo e que volte sempre!!! Ahh!!! E não esqueça de conferir os cursos em vídeos em videoaulas!!! </p>
+                    
+                </a>
+                
+                <a href="#" class="list-group-item list-group-item-action">
+            
+            		<div class="d-flex w-100 justify-content-between">
+                		<h5>Titulo da lista</h5>
+                    	<small>Tres dias atrás</small>
+                    </div>
+                    
+                	<p class="mb-1">Desfrute de todos os recursos oferecidos por este portal seja você meu aluno, ex-aluno, amigo ou um internauta. Fique a vontade para entrar em contato sempre quando quiser pois respondo a todos as mensagens. Procuro manter sempre o material gratuito, essa é a minha idéia!!! Portanto, para que eu possa continuar, não deixe de se inscrever no canal do you tube e curtir a minha página no facebook. Espero que possa ajudar a encontrar o que procura!!! Tenho como objetivo dividir um pouco do meu conhecimento com você, seja ou não aluno dos cursos ou instituições que leciono. Espero que goste do conteúdo e que volte sempre!!! Ahh!!! E não esqueça de conferir os cursos em vídeos em videoaulas!!! </p>
+                    
+                </a>
+            
+            </div>
+        
+        </div>
+        
+        
+        <br>
+        <br>
+        
+        <div class="col-8">
+        
+        	<div class="row">
+            
+            
+            	<div class="col-3">
+            		<div class="list-group" id="list-tab" role="tablist">
+                     	<a href="#list-home" class="list-group-item list-group-item-action active" id="Primeiro" data-toggle="list" role="tab" aria-control="home">Primeiro</a>                
+            		</div>
+            	</div>
+            
+            
+            
+            	<div class="col-9">            
+            		<div class="tab-content" id="nav-tabContent">
+                   		<div id="list-home" class="tab-pane fade show active" role="tabpanel" aria-labelledby="Primeiro">
+                    
+                    	<h3>Primeiro</h3>
+                        <p>
+                        Desfrute de todos os recursos oferecidos por este portal seja você meu aluno, ex-aluno, amigo ou um internauta. Fique a vontade para entrar em contato sempre quando quiser pois respondo a todos as mensagens. Procuro manter sempre o material gratuito, essa é a minha idéia!!! Portanto, para que eu possa continuar, não deixe de se inscrever no canal do you tube e curtir a minha página no facebook. Espero que possa ajudar a encontrar o que procura!!! Tenho como objetivo dividir um pouco do meu conhecimento com você, seja ou não aluno dos cursos ou instituições que leciono. Espero que goste do conteúdo e que volte sempre!!! Ahh!!! E não esqueça de conferir os cursos em vídeos em videoaulas!!! 
+                        </p>
+                    
+                    	</div>                
+                	</div>
+            	</div>
+            
+            
+            
+            </div>
+            
+            
+            <div class="row">
+            
+            	<div class="col-3">
+                
+                	<!-- DISPARADORES -->
+            		<div class="list-group" id="list-tab" role="tablist">
+                     	<a href="#Primeiro" class="list-group-item list-group-item-action active" id="Primeiro" data-toggle="list" role="tab" aria-control="home">Primeiro</a> 
+                        <a href="#Segundo" class="list-group-item list-group-item-action" id="Segundo" data-toggle="list" role="tab" aria-control="Segundo">Segundo</a>               
+            		</div>
+            	</div>
+            
+            
+            
+            	<div class="col-9">            
+            		<div class="tab-content" id="nav-tabContent">
+                   		<div id="Primeiro" class="tab-pane fade show active" role="tabpanel" aria-labelledby="Primeiro">
+                    
+                    		<h3>Primeiro</h3>
+                        	<p>
+                        Desfrute de todos os recursos oferecidos por este portal seja você meu aluno, ex-aluno, amigo ou um internauta. Fique a vontade para entrar em contato sempre quando quiser pois respondo a todos as mensagens. Procuro manter sempre o material gratuito, essa é a minha idéia!!! Portanto, para que eu possa continuar, não deixe de se inscrever no canal do you tube e curtir a minha página no facebook. Espero que possa ajudar a encontrar o que procura!!! Tenho como objetivo dividir um pouco do meu conhecimento com você, seja ou não aluno dos cursos ou instituições que leciono. Espero que goste do conteúdo e que volte sempre!!! Ahh!!! E não esqueça de conferir os cursos em vídeos em videoaulas!!! 
+                        	</p>
+                    
+                    	</div>
+                        
+                        
+                        <div id="Segundo" class="tab-pane fade" role="tabpanel" aria-labelledby="Segundo">
+                    
+                    		<h3>Segundo</h3>
+                        	<p>
+                        Desfrute de todos os recursos oferecidos por este portal seja você meu aluno, ex-aluno, amigo ou um internauta. Fique a vontade para entrar em contato sempre quando quiser pois respondo a todos as mensagens. Procuro manter sempre o material gratuito, essa é a minha idéia!!! Portanto, para que eu possa continuar, não deixe de se inscrever no canal do you tube e curtir a minha página no facebook. Espero que possa ajudar a encontrar o que procura!!! Tenho como objetivo dividir um pouco do meu conhecimento com você, seja ou não aluno dos cursos ou instituições que leciono. Espero que goste do conteúdo e que volte sempre!!! Ahh!!! E não esqueça de conferir os cursos em vídeos em videoaulas!!! 
+                        	</p>
+                    
+                    	</div>
+                                       
+                	</div>
+            	</div>
+            
+            </div>
+                      
+        </div>
+    
+    </div>
+ 
+</div>
+
+
+
+<?php
+
+	include_once "footer.php";
+
+?>
