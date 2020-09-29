680b18fb24111e140b80f9e7f16fdb464f85778d
Carlos Muniz <professorcarlosmuniz@gmail.com>
2020-09-29 15:41:40 -0300

    Add files via upload

---
+++ comp_listgroup_1.php
@@ -0,0 +1,212 @@
+<?php
+
+	include_once "topo.php";
+
+?>
+
+<!-- LISTGROUP -->
+
+
+<div class="container">
+    
+    <div class="row">
+    
+    	<div class="col-4">
+                
+        	<!-- LISTGROUP -->
+            
+            <ul class="list-group">
+            
+            	<li class="list-group-item">Item 1</li>
+                <li class="list-group-item">Item 2</li>
+                <li class="list-group-item">Item 3</li>
+                <li class="list-group-item">Item 4</li>
+                <li class="list-group-item">Item 5</li>
+                <li class="list-group-item">Item 6</li>
+                <li class="list-group-item">Item 7</li>
+                <li class="list-group-item">Item 8</li>
+                <li class="list-group-item">Item 9</li>
+           
+            
+            </ul>
+           
+                    	
+        </div>
+    
+    </div>
+ 
+</div>
+
+<br>
+
+<!-- LISTGROUP -->
+
+
+<div class="container">
+    
+    <div class="row">
+    
+    	<div class="col-4">
+                
+        	<!-- LISTGROUP -->
+            
+            <ul class="list-group">
+            
+            	<li class="list-group-item">Item 1</li>
+                <li class="list-group-item">Item 2</li>
+                <li class="list-group-item active">Item 3</li>
+                <li class="list-group-item">Item 4</li>
+                <li class="list-group-item">Item 5</li>
+                <li class="list-group-item">Item 6</li>
+                <li class="list-group-item">Item 7</li>
+                <li class="list-group-item">Item 8</li>
+                <li class="list-group-item">Item 9</li>
+           
+            
+            </ul>
+           
+                    	
+        </div>
+    
+    </div>
+ 
+</div>
+
+<br>
+
+<!-- LISTGROUP -->
+
+
+<div class="container">
+    
+    <div class="row">
+    
+    	<div class="col-4">
+                
+        	<!-- LISTGROUP -->
+            
+            <ul class="list-group">
+            
+            	<li class="list-group-item">Item 1</li>
+                <li class="list-group-item">Item 2</li>
+                <li class="list-group-item active">Item 3</li>
+                <li class="list-group-item disabled">Item 4</li>
+                <li class="list-group-item">Item 5</li>
+                <li class="list-group-item">Item 6</li>
+                <li class="list-group-item">Item 7</li>
+                <li class="list-group-item">Item 8</li>
+                <li class="list-group-item">Item 9</li>
+           
+            
+            </ul>
+           
+                    	
+        </div>
+    
+    </div>
+ 
+</div>
+
+<br>
+
+<div class="container">
+    
+    <div class="row">
+    
+    	<div class="col-4">
+                
+        	<!-- LISTGROUP COM EFEITOS DE LINKS-->
+            
+            <div class="list-group">
+            
+            	<a href="#" class="list-group-item">Item 1</a>
+                <a href="#" class="list-group-item">Item 2</a>
+                <a href="#" class="list-group-item active">Item 3</a>
+                <a href="#" class="list-group-item disabled">Item 4</a>
+                <a href="#" class="list-group-item">Item 5</a>
+                <a href="#" class="list-group-item">Item 6</a>
+                <a href="#" class="list-group-item">Item 7</a>
+                <a href="#" class="list-group-item">Item 8</a>
+                <a href="#" class="list-group-item">Item 9</a>
+           
+            
+            </div>
+           
+                    	
+        </div>
+    
+    </div>
+ 
+</div>
+
+
+<br>
+
+<div class="container">
+    
+    <div class="row">
+    
+    	<div class="col-4">
+                
+        	<!-- LISTGROUP COM EFEITOS DE LINKS com HOUVER-->
+            
+            <div class="list-group">
+            
+            	<a href="#" class="list-group-item list-group-item-action">Item 1</a>
+                <a href="#" class="list-group-item list-group-item-action">Item 2</a>
+                <a href="#" class="list-group-item active list-group-item-action">Item 3</a>
+                <a href="#" class="list-group-item disabled list-group-item-action">Item 4</a>
+                <a href="#" class="list-group-item list-group-item-action">Item 5</a>
+                <a href="#" class="list-group-item list-group-item-action">Item 6</a>
+                <a href="#" class="list-group-item list-group-item-action">Item 7</a>
+                <a href="#" class="list-group-item list-group-item-action">Item 8</a>
+                <a href="#" class="list-group-item list-group-item-action">Item 9</a>
+           
+            
+            </div>
+           
+                    	
+        </div>
+    
+    </div>
+ 
+</div>
+
+<br>
+
+<div class="container">
+    
+    <div class="row">
+    
+    	<div class="col-4">
+                
+        	<!-- LISTGROUP COM EFEITOS DE LINKS com HOUVER-->
+            
+            <div class="list-group">
+            
+            	<button type="button" class="list-group-item list-group-item-action">Botão 1</button>
+                <button type="button" class="list-group-item list-group-item-action">Botão 2</button>
+                <button type="button" class="list-group-item active list-group-item-action">Botão 3</button>
+                <button type="button" class="list-group-item list-group-item-action" disabled>Botão 4</button>
+                <button type="button" class="list-group-item list-group-item-action">Botão 5</button>
+                <button type="button" class="list-group-item list-group-item-action">Botão 6</button>
+                <button type="button" class="list-group-item list-group-item-action">Botão 7</button>
+                <button type="button" class="list-group-item list-group-item-action">Botão 8</button>
+                <button type="button" class="list-group-item list-group-item-action">Botão 9</button>
+            
+            </div>
+           
+                    	
+        </div>
+    
+    </div>
+ 
+</div>
+
+
+
+<?php
+
+	include_once "footer.php";
+
+?>
