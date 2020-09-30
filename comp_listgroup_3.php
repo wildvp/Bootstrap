968f7ae725104e254ec60daedc5cfa35e6fd20d2
Carlos Muniz <professorcarlosmuniz@gmail.com>
2020-09-30 11:21:21 -0300

    Add files via upload

---
+++ comp_listgroup_3.php
@@ -0,0 +1,64 @@
+<?php
+
+	include_once "topo.php";
+
+?>
+
+<!-- LISTGROUP 2-->
+
+
+<div class="container">
+    
+    <div class="row">
+    
+    	<div class="col-4">
+        
+        	<div class="list-group">
+            
+            	<a href="#" class="list-group-item list-group-item-action">
+                	<div class="d-flex w-100 justify-content-between">
+                    	<h5>Obrigado por sua visita!!!</h5>
+                        <small>Professor Carlos Muniz</small>
+                    </div>
+                    
+                    <p align="justify" class="mb-1">Desfrute de todos os recursos oferecidos por este portal seja você meu aluno, ex-aluno, amigo ou um internauta. Fique a vontade para entrar em contato sempre quando quiser pois respondo a todos as mensagens. Procuro manter sempre o material gratuito, essa é a minha idéia!!! Portanto, para que eu possa continuar, não deixe de se inscrever no canal do you tube e curtir a minha página no facebook. Espero que possa ajudar a encontrar o que procura!!! Tenho como objetivo dividir um pouco do meu conhecimento com você, seja ou não aluno dos cursos ou instituições que leciono. Espero que goste do conteúdo e que volte sempre!!! Ahh!!! E não esqueça de conferir os cursos em vídeos em videoaulas!!!
+                    </p>
+                </a>
+                
+                <a href="#" class="list-group-item list-group-item-action active">
+                	<div class="d-flex w-100 justify-content-between">
+                    	<h5>Obrigado por sua visita!!!</h5>
+                        <small>Professor Carlos Muniz</small>
+                    </div>
+                    
+                    <p align="justify" class="mb-1">Desfrute de todos os recursos oferecidos por este portal seja você meu aluno, ex-aluno, amigo ou um internauta. Fique a vontade para entrar em contato sempre quando quiser pois respondo a todos as mensagens. Procuro manter sempre o material gratuito, essa é a minha idéia!!! Portanto, para que eu possa continuar, não deixe de se inscrever no canal do you tube e curtir a minha página no facebook. Espero que possa ajudar a encontrar o que procura!!! Tenho como objetivo dividir um pouco do meu conhecimento com você, seja ou não aluno dos cursos ou instituições que leciono. Espero que goste do conteúdo e que volte sempre!!! Ahh!!! E não esqueça de conferir os cursos em vídeos em videoaulas!!!
+                    </p>
+                </a>
+                
+                <a href="#" class="list-group-item list-group-item-action">
+                	<div class="d-flex w-100 justify-content-between">
+                    	<h5>Obrigado por sua visita!!!</h5>
+                        <small>Professor Carlos Muniz</small>
+                    </div>
+                    
+                    <p align="justify" class="mb-1">Desfrute de todos os recursos oferecidos por este portal seja você meu aluno, ex-aluno, amigo ou um internauta. Fique a vontade para entrar em contato sempre quando quiser pois respondo a todos as mensagens. Procuro manter sempre o material gratuito, essa é a minha idéia!!! Portanto, para que eu possa continuar, não deixe de se inscrever no canal do you tube e curtir a minha página no facebook. Espero que possa ajudar a encontrar o que procura!!! Tenho como objetivo dividir um pouco do meu conhecimento com você, seja ou não aluno dos cursos ou instituições que leciono. Espero que goste do conteúdo e que volte sempre!!! Ahh!!! E não esqueça de conferir os cursos em vídeos em videoaulas!!!
+                    </p>
+                </a>
+            	            
+            </div>
+            
+            
+                              
+        </div>
+    
+    </div>
+ 
+</div>
+
+
+
+<?php
+
+	include_once "footer.php";
+
+?>
---
+++ comp_listgroup_4.php
@@ -0,0 +1,90 @@
+<?php
+
+	include_once "topo.php";
+
+?>
+
+<!-- LISTGROUP 2-->
+
+
+<div class="container">
+    
+    <div class="row">
+    
+    	<div class="col-12">
+        
+        	<div class="row">
+            
+            <!--DISPARADORES-->
+               	<div class="col-3">
+                            
+            		<div class="list-group" id="list-tab" role="tablist">
+                		<a href="#list-home" class="list-group-item list-group-item-action active" id="list-home-list" data-toggle="list" role="tab" aria-controls="home">Home</a>
+                	</div>
+                    
+                    <div class="list-group" id="list-tab" role="tablist">
+                		<a href="#list-perfil" class="list-group-item list-group-item-action" id="list-perfil-list" data-toggle="list" role="tab" aria-controls="perfil">Perfil</a>
+                	</div>
+                                        
+                    <div class="list-group" id="list-tab" role="tablist">
+                		<a href="#list-cursos" class="list-group-item list-group-item-action" id="list-cursos-list" data-toggle="list" role="tab" aria-controls="cursos">Cursos</a>
+                	</div>
+                    
+                    <div class="list-group" id="list-tab" role="tablist">
+                		<a href="#list-msg" class="list-group-item list-group-item-action" id="list-msg-list" data-toggle="list" role="tab" aria-controls="msg">Mensagem</a>
+                	</div>
+                    
+                </div>
+                
+                
+                <!--CONTEUDOS-->
+                <div class="col-9">
+                	<div class="tab-content" id="nav-tabContent">
+                    	<div id="list-home" class="tab-pane fade show active" role="tabpanel" aria-labelledby="list-home-list">
+                        	<h3>Home</h3>
+                            <p align="justify">
+                            Desfrute de todos os recursos oferecidos por este portal seja você meu aluno, ex-aluno, amigo ou um internauta. Fique a vontade para entrar em contato sempre quando quiser pois respondo a todos as mensagens. Procuro manter sempre o material gratuito, essa é a minha idéia!!! Portanto, para que eu possa continuar, não deixe de se inscrever no canal do you tube e curtir a minha página no facebook. Espero que possa ajudar a encontrar o que procura!!! Tenho como objetivo dividir um pouco do meu conhecimento com você, seja ou não aluno dos cursos ou instituições que leciono. Espero que goste do conteúdo e que volte sempre!!! Ahh!!! E não esqueça de conferir os cursos em vídeos em videoaulas!!!
+                            </p>
+                    	</div>
+                        
+                        <div id="list-perfil" class="tab-pane fade" role="tabpanel" aria-labelledby="list-perfil-list">
+                        	<h3>Perfil</h3>
+                            <p align="justify">
+                            Segue abaixo, um vídeo de abertura para agradecer a sua visita. Se adquiriu um novo computador, assista-o e tomará conhecimento de muitos benefícios que as pessoas normalmente desconhecem. Acredite, ATÉ MESMO PESSOAS DA ÁREA DE TI. Decidi colocar o vídeo abaixo na abertura para que tenha uma pequena amostra do portal. Normalmente, quando me chamam para fazer a formatação de um computador, depois que o mesmo esta pronto, recebo muitas perguntas de meus cliente. Sempre as respondo, mas acredito que, quando vou embora, surgem outras dúvidas. Por isso, recomendo que assista a esse vídeo inicial e, se for completamente leigo no assunto (ou quiser enriquecer seu conhecimento, comece pelo curso de windows10 em videoaulas clicando aqui.
+                            </p>
+                    	</div>
+                        
+                        <div id="list-cursos" class="tab-pane fade" role="tabpanel" aria-labelledby="list-cursos-list">
+                        	<h3>Cursos</h3>
+                            <p align="justify">
+                            Segue abaixo, um vídeo de abertura para agradecer a sua visita. Se adquiriu um novo computador, assista-o e tomará conhecimento de muitos benefícios que as pessoas normalmente desconhecem. Acredite, ATÉ MESMO PESSOAS DA ÁREA DE TI. Decidi colocar o vídeo abaixo na abertura para que tenha uma pequena amostra do portal. Normalmente, quando me chamam para fazer a formatação de um computador, depois que o mesmo esta pronto, recebo muitas perguntas de meus cliente. Sempre as respondo, mas acredito que, quando vou embora, surgem outras dúvidas. Por isso, recomendo que assista a esse vídeo inicial e, se for completamente leigo no assunto (ou quiser enriquecer seu conhecimento, comece pelo curso de windows10 em videoaulas clicando aqui.
+                            </p>
+                    	</div>
+                        
+                        <div id="list-msg" class="tab-pane fade" role="tabpanel" aria-labelledby="list-msg-list">
+                        	<h3>Mensagem</h3>
+                            <p align="justify">
+                            Segue abaixo, um vídeo de abertura para agradecer a sua visita. Se adquiriu um novo computador, assista-o e tomará conhecimento de muitos benefícios que as pessoas normalmente desconhecem. Acredite, ATÉ MESMO PESSOAS DA ÁREA DE TI. Decidi colocar o vídeo abaixo na abertura para que tenha uma pequena amostra do portal. Normalmente, quando me chamam para fazer a formatação de um computador, depois que o mesmo esta pronto, recebo muitas perguntas de meus cliente. Sempre as respondo, mas acredito que, quando vou embora, surgem outras dúvidas. Por isso, recomendo que assista a esse vídeo inicial e, se for completamente leigo no assunto (ou quiser enriquecer seu conhecimento, comece pelo curso de windows10 em videoaulas clicando aqui.
+                            </p>
+                    	</div>
+                    </div>
+                </div>
+            
+            	
+            
+            </div> 
+                 
+                              
+        </div>
+    
+    </div>
+ 
+</div>
+
+
+
+<?php
+
+	include_once "footer.php";
+
+?>
