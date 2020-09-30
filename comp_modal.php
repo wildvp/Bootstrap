0c94e45e725800b6a3ad38dd4cce477b8f9c44db
Carlos Muniz <professorcarlosmuniz@gmail.com>
2020-09-30 16:18:44 -0300

    Add files via upload

---
+++ comp_modal.php
@@ -0,0 +1,146 @@
+<?php
+
+	include_once "topo.php";
+
+?>
+
+<!-- MODAL-->
+
+
+<div class="container">
+    
+    <div class="row">
+    
+    	<div class="col-12">
+        
+        	<!-- DISPARADOR DO MODAL-->
+            
+            <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#modalInformativo">
+            	Disparador do modal
+            </button>
+        
+        	
+        
+        	<div id="modalInformativo" class="modal fade">
+        
+        				<div class="modal-dialog" role="document">
+            				<div class="modal-content">
+                					<div class="modal-header">
+                    					<h5 class="modal-title">Titulo do Modal</h5>
+                        				<button type="button" class="close" data-dismiss="modal" aria-label="Fechar">
+                        					<span aria-hidden="true">&times;</span>
+                        				</button>
+                    				</div>
+                    
+                    				<div class="modal-body">
+                    					<p>Corpo do Modal</p>
+                    				</div>
+                    
+                    				<div class="modal-footer">
+                    					<button type="button" class="btn btn-primary">Salvar Alterações</button>
+                        				<button type="button" class="btn btn-danger" data-dismiss="modal">Cancelar</button>	                        
+                    				</div>
+            				</div>
+            			</div>
+            
+            </div>	
+        
+        </div>
+      
+    </div>
+    
+    
+    
+    
+    
+    <div class="row">
+    
+    	<div class="col-12">
+        
+        	<!-- DISPARADOR DO MODAL-->
+            
+            <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#modalInformativo1">
+            	Disparador do modal grande
+            </button>
+        
+        	
+        
+        	<div id="modalInformativo1" class="modal fade">
+        
+        				<div class="modal-dialog modal-lg" role="document">
+            				<div class="modal-content">
+                					<div class="modal-header">
+                    					<h5 class="modal-title">Titulo do Modal</h5>
+                        				<button type="button" class="close" data-dismiss="modal" aria-label="Fechar">
+                        					<span aria-hidden="true">&times;</span>
+                        				</button>
+                    				</div>
+                    
+                    				<div class="modal-body">
+                    					<p>Corpo do Modal</p>
+                    				</div>
+                    
+                    				<div class="modal-footer">
+                    					<button type="button" class="btn btn-primary">Salvar Alterações</button>
+                        				<button type="button" class="btn btn-danger" data-dismiss="modal">Cancelar</button>	                        
+                    				</div>
+            				</div>
+            			</div>
+            
+            </div>	
+        
+        </div>
+      
+    </div>
+    
+    
+    
+    <div class="row">
+    
+    	<div class="col-12">
+        
+        	<!-- DISPARADOR DO MODAL-->
+            
+            <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#modalInformativo2">
+            	Disparador do modal pequeno
+            </button>
+        
+        	
+        
+        	<div id="modalInformativo2" class="modal fade">
+        
+        				<div class="modal-dialog modal-sm" role="document">
+            				<div class="modal-content">
+                					<div class="modal-header">
+                    					<h5 class="modal-title">Titulo do Modal</h5>
+                        				<button type="button" class="close" data-dismiss="modal" aria-label="Fechar">
+                        					<span aria-hidden="true">&times;</span>
+                        				</button>
+                    				</div>
+                    
+                    				<div class="modal-body">
+                    					<p>Corpo do Modal</p>
+                    				</div>
+                    
+                    				<div class="modal-footer">
+                    					<button type="button" class="btn btn-primary">Salvar Alterações</button>
+                        				<button type="button" class="btn btn-danger" data-dismiss="modal">Cancelar</button>	                        
+                    				</div>
+            				</div>
+            			</div>
+            
+            </div>	
+        
+        </div>
+      
+    </div>
+ 
+</div>
+
+
+
+<?php
+
+	include_once "footer.php";
+
+?>
