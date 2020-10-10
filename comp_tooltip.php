7c1afff2f8dc5f225916001f0b78990de752ea18
Carlos Muniz <professorcarlosmuniz@gmail.com>
2020-10-10 19:16:41 -0300

    Add files via upload

---
+++ comp_tooltip.php
@@ -0,0 +1,55 @@
+<?php
+
+	include_once "topo.php";
+
+?>
+
+
+
+
+
+
+	<div class="container">
+    
+    	<div class="row">
+        
+        	<div class="col-12">
+            
+            	<!--TOOLTIPS -->
+                
+                <br>
+                <br>
+                <button type="button" class="btn btn-secondary" data-toggle="tooltip" data-placement="top" title="Tooltip no Topo">
+                Tooltip Top
+                </button>
+                <br>
+                <br>
+                 <button type="button" class="btn btn-secondary" data-toggle="tooltip" data-placement="right" title="Tooltip no Topo">
+                Tooltip Right
+                </button>
+                <br>
+                <br>
+                 <button type="button" class="btn btn-secondary" data-toggle="tooltip" data-placement="left" title="Tooltip no Topo">
+                Tooltip Left
+                </button>
+                
+                <br>
+                <br>
+                
+                <button type="button" class="btn btn-secondary" data-toggle="tooltip" data-placement="bottom" title="Tooltip no Topo">
+                Tooltip Bottom
+                </button>
+                
+                
+            
+            </div>
+        
+        </div>
+    
+    </div>
+
+<?php
+
+	include_once "footer.php";
+
+?>
