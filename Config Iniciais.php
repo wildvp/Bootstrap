cb49dbb9dc722a752a83874a0a13e9e98490e405
Carlos Muniz <professorcarlosmuniz@gmail.com>
2020-11-18 12:03:22 -0300

    Add files via upload

--- Config Iniciais.php
+++ Config Iniciais.php
@@ -1,58 +1,160 @@
-<?php
-
-	// Chamando a página topo
-
-?>
-
-<!--CONCIFURAÇÕES INICIAIS -->
-<!DOCTYPE html>
-<html lang="pt-br">
-<head>
-
-	<charset="utf-8">
-	<meta name="viewport" content="width-device-width, initial-sacale = 1, shrink-to-fit=no">
-	<title>Configurações Iniciais</title>
-    <link rel="stylesheet" href="css/bootstrap.min.css">
-
-</head>
-
-<body>
-
-
-	<h1> Olá Mundo!!! </h1>
-
-
-
-
-
-
-	<!-- PLUGINS BOOTSTRAP-->
-    <!-- JQuery -->
-    <script src="js/jquery-3.5.1.min.js"></script>
-    <!-- Popper -->
-    <script src="js/popper.min.js"></script>
-    <!-- Javascript -->
-    <script src="js/bootstrap.min.js"></script>
-	
-   
-
-
-</body>
-
-</html>
-
-
-
-
-
-
-
-
-
-
-
-<?php
-
-	// Chamando a página rodapé
-
+<?php
+
+	// Chamando a página topo
+
+?>
+
+<!--CONCIFURAÇÕES INICIAIS -->
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+
+	<charset="utf-8">
+	<meta name="viewport" content="width-device-width, initial-sacale = 1, shrink-to-fit=no">
+	<title>Configurações Iniciais</title>
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+
+</head>
+
+<body>
+
+
+	<div class="container">
+    
+    	<div class="row">
+        
+        	<div class="col-12">
+            
+            	<!--CAROUSSEL DE IMAGENS -->
+                
+                <div id="ControleCarrrossel" class="carousel slide" data-ride="carousel">
+                
+                	<!--INDICADORES -->
+                    <ol class="carousel-indicators">
+                    	<li data-target="#ControleCarrrossel" data-slide-to="0" class="active"></li>
+                    	<li data-target="#ControleCarrrossel" data-slide-to="1"></li>
+                        <li data-target="#ControleCarrrossel" data-slide-to="2"></li>
+                        <li data-target="#ControleCarrrossel" data-slide-to="3"></li>
+                        <li data-target="#ControleCarrrossel" data-slide-to="4"></li>
+                        <li data-target="#ControleCarrrossel" data-slide-to="5"></li>
+                    </ol>
+                
+                
+                
+                	<div class="carousel-inner">
+                    
+                    	<!--IMAGENS -->
+                    
+                    	<div class="carousel-item active">
+                            <img class="d-block w-100" src="images/jpg/imagem1.jpg">
+                            <!--LEGENDA DA FOTO-->
+                            <div class="carousel-caption d-none d-md-block">
+                            	<h3>Local da foto</h3>
+                                <p>Descrição da foto</p>
+                            </div>
+                    	</div>
+                        
+                        <div class="carousel-item">
+                        	<img class="d-block w-100" src="images/jpg/imagem2.jpg">
+                            <div class="carousel-caption d-none d-md-block">
+                            	<h3>Local da foto</h3>
+                                <p>Descrição da foto</p>
+                            </div>
+                    	</div>
+                        
+                       	<div class="carousel-item">
+                        	<img class="d-block w-100" src="images/jpg/imagem3.jpg">
+                            <div class="carousel-caption d-none d-md-block">
+                            	<h3>Local da foto</h3>
+                                <p>Descrição da foto</p>
+                            </div>
+                    	</div>
+                        
+                        <div class="carousel-item">
+                        	<img class="d-block w-100" src="images/jpg/imagem4.jpg">
+                            <div class="carousel-caption d-none d-md-block">
+                            	<h3>Local da foto</h3>
+                                <p>Descrição da foto</p>
+                            </div>
+                    	</div>
+                        
+                        <div class="carousel-item">
+                        	<img class="d-block w-100" src="images/jpg/imagem5.jpg">
+                            <div class="carousel-caption d-none d-md-block">
+                            	<h3>Local da foto</h3>
+                                <p>Descrição da foto</p>
+                            </div>
+                    	</div>
+                        
+                        <div class="carousel-item">
+                        	<img class="d-block w-100" src="images/jpg/imagem6.jpg">
+                            <div class="carousel-caption d-none d-md-block">
+                            	<h3>Local da foto</h3>
+                                <p>Descrição da foto</p>
+                            </div>
+                    	</div>
+                        
+                        <!--BOTOES -->
+                        <a href="#ControleCarrrossel" class="carousel-control-prev" role="button" data-slide="prev">
+                        	<span class="carousel-control-prev-icon" aria-hidden="true"</span>
+                            <span class="sr-only">Voltar</span>
+                        </a>
+                        
+                        <a href="#ControleCarrrossel" class="carousel-control-next" role="button" data-slide="next">
+                        	<span class="carousel-control-next-icon" aria-hidden="true"</span>
+                            <span class="sr-only">Voltar</span>
+                        </a>
+                        
+                    
+                    </div>
+                
+                </div>
+            
+            </div>
+        
+        </div>
+    
+    </div>
+
+
+
+
+
+
+	<!-- PLUGINS BOOTSTRAP-->
+    <!-- JQuery -->
+    <script src="js/jquery-3.5.1.min.js"></script>
+    <!-- Popper -->
+    <script src="js/popper.min.js"></script>
+    <!-- Javascript -->
+    <script src="js/bootstrap.min.js"></script>
+    <!--VELOCIDADE CAROUSEL-->
+    <script type="text/javascript">
+		$('.carousel').carousel({
+			interval:4000
+		})
+	</script>
+    
+	
+   
+
+
+</body>
+
+</html>
+
+
+
+
+
+
+
+
+
+
+
+<?php
+
+	// Chamando a página rodapé
+
 ?>
